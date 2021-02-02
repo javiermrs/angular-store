00966df0336ce923948ed9beafef4c57586df878
Javier <javiermrs26@gmail.com>
2021-02-02 17:32:32 -0400

    finalizing styles to footer and contact page

--- src/app/contact/contact.module.ts
+++ src/app/contact/contact.module.ts
@@ -4,13 +4,31 @@ import { CommonModule } from '@angular/common';
 import { ContactComponent } from './components/contact/contact.component';
 import { ContactRoutingModule } from './contact-routing.module';
 
+import { ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from '../material/material.module';
+// import { NavComponent } from '../components/nav/nav.component'
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list'
+
 @NgModule({
     declarations: [
         ContactComponent
     ],
    imports: [
         CommonModule,
-        ContactRoutingModule
+        ContactRoutingModule,
+        ReactiveFormsModule,
+        MaterialModule,
+        LayoutModule,
+        MatToolbarModule,
+        MatButtonModule,
+        MatSidenavModule,
+        MatIconModule,
+        MatListModule
     ]
 })
 export class ContactModule {}
